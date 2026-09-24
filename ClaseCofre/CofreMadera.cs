2ed73fbb1167a3d34c3bedafdfbe6ef701aab54c
SilverShadow571 <nosores9@gmail.com>
2026-09-23 21:03:34 -0300

    Gitignore + program.cs + clase cofreMadera
    Se creo el gitignore, el program.cs y clase cofreMadera

---
+++ ClaseCofre/CofreMadera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ClaseCofre
+{
+    internal class CofreMadera
+    {
+        private int oro = 75;
+        private bool abierto = false;
+
+        public void Abrir()
+        {
+            if (abierto == false) 
+                Console.WriteLine($"¡Abriste el cofre encontraste {oro}G!.");
+            else
+                Console.WriteLine("El cofre ya esta abierto.");
+        }
+    }
+}
---
+++ ClaseCofre/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClaseCofre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
