add249d326a96a065c816452b65def91858219c2
SilverShadow571 <nosores9@gmail.com>
2026-09-23 22:24:33 -0300

    Descripcion Main
    Se añadio descripcion en el main

--- ClaseCofre/Program.cs
+++ ClaseCofre/Program.cs
@@ -5,8 +5,10 @@
         static void Main(string[] args)
         {
             CofreMadera cofreMadera = new CofreMadera();
+            //Se abre el cofre por primera vez.
             cofreMadera.Abrir();
             Console.ReadKey();
+            //La segunda vez el cofre ya esta vacio.
             cofreMadera.Abrir();
             Console.ReadKey();
         }
